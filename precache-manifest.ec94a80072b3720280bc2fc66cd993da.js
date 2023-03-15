6b94d883a68d5bce45d0a4f45037e5466375de81
Sneha <sneha.squasher@gmail.com>
2023-03-15 19:10:04 +0800

    Updates

---
+++ precache-manifest.ec94a80072b3720280bc2fc66cd993da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fce6ca0bd68f7842671e",
+    "url": "/spurr/static/css/main.71646641.chunk.css"
+  },
+  {
+    "revision": "fce6ca0bd68f7842671e",
+    "url": "/spurr/static/js/main.fce6ca0b.chunk.js"
+  },
+  {
+    "revision": "bc0a9f4a9965b496cdf7",
+    "url": "/spurr/static/js/1.bc0a9f4a.chunk.js"
+  },
+  {
+    "revision": "7c29cda7203d8cfd7dc6",
+    "url": "/spurr/static/js/runtime~main.7c29cda7.js"
+  },
+  {
+    "revision": "151d60b50840f3f831a94e4284d61a6e",
+    "url": "/spurr/static/media/phone_screen_1.151d60b5.png"
+  },
+  {
+    "revision": "3b3d72903ca7a3f7ccff9f697bb75ef9",
+    "url": "/spurr/index.html"
+  }
+];
